6235c902e8a7ad677020e8cababf45dffd419624
MaxtorCoder <warsongkiller.s8@gmail.com>
2022-10-31 12:19:12 +0100

    Add listfile loading and some UI changes

---
+++ Wow.DB2DefinitionDumper/ListfileReader.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+namespace Wow.DB2DefinitionDumper
+{
+    public class ListfileReader
+    {
+        public bool IsLoaded { get; private set; } = false;
+
+        private Dictionary<uint, string> _listfileEntries = new();
+
+        /// <summary>
+        /// Opens the listfile file and loads that into memory.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task ReadFileAsync(string path)
+        {
+            using var reader = new StreamReader(path);
+
+            while (!reader.EndOfStream)
+            {
+                var line = await reader.ReadLineAsync();
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                var split = line.Split(";");
+                if (split.Length < 2)
+                    continue;
+
+                if (!uint.TryParse(split[0], out var fileDataId))
+                    continue;
+
+                _listfileEntries.Add(fileDataId, split[1]);
+            }
+
+            IsLoaded = true;
+        }
+
+        /// <summary>
+        /// Retrieves all DB2 filenames
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<uint, string> GetAvailableDB2s()
+        {
+            return _listfileEntries.Where(x => x.Value.EndsWith(".db2"))
+                .OrderBy(x => x.Value, StringComparer.Ordinal)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        /// <summary>
+        /// Returns the FileDataID for a specific entry in storage.
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns></returns>
+        public uint GetFileDataIdByEntry(string entry)
+            => _listfileEntries.FirstOrDefault(x => x.Value == entry).Key;
+    }
+}
--- Wow.DB2DefinitionDumper/MainWindow.xaml.cs
+++ Wow.DB2DefinitionDumper/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text.Json;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+
 using Wow.DB2DefinitionDumper.DBD;
 using Wow.DB2DefinitionDumper.Providers;
 
@@ -22,9 +15,15 @@ namespace Wow.DB2DefinitionDumper
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private readonly ListfileReader _listfileReader = new();
+
+        private List<DB2DisplayNameRecord> _db2DisplayNames = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadDisplayNames();
 
             ToolBar.MouseLeftButtonDown += (e, o) => DragMove();
             CloseBtn.Click += (e, o) => Close();
@@ -33,38 +32,117 @@ namespace Wow.DB2DefinitionDumper
         private async void DumpButton_Click(object sender, RoutedEventArgs e)
         {
             var tableName = TableName.Text;
-            if (string.IsNullOrEmpty(tableName))
+            if (_listfileReader.IsLoaded)
+                tableName = TableDropdown.SelectedItem as string;
+
+            var normalizedName = _db2DisplayNames.FirstOrDefault(x => x.displayName == tableName);
+            if (normalizedName == null)
             {
-                ResultBox.Text = "Invalid table name";
+                _ = MessageBox.Show($"Failed to find normalized name for {tableName}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(normalizedName.displayName))
+            {
+                _ = MessageBox.Show("Invalid table name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
             var version = VersionBox.Text;
             if (string.IsNullOrEmpty(version))
             {
-                ResultBox.Text = "Please provide a valid build. Like 10.0.2.45969";
+                _ = MessageBox.Show("Please provide a valid build. Like 10.0.2.45969", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
-            string[] array = version.Split(new char[1] { '.' });
-            if (array.Count() < 4)
+            var array = version.Split('.');
+            if (array.Length < 4)
             {
-                ResultBox.Text = "Please provide a valid build. Like 10.0.2.45969";
+                _ = MessageBox.Show("Please provide a valid build. Like 10.0.2.45969", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
             try
             {
-
                 var dbdProvider = new GithubDbdProvider();
-                var dbdStream = await dbdProvider.StreamForTableName(tableName);
+                var dbdStream = await dbdProvider.StreamForTableName(normalizedName.displayName);
 
-                var dbdInfo = DbdBuilder.Build(dbdStream, tableName, version);
-                ResultBox.Text = dbdInfo.GetColumns() + Environment.NewLine + dbdInfo.DumpMeta();
-            } catch (Exception ex)
+                var fileDataId = 0u;
+                if (_listfileReader.IsLoaded)
+                    fileDataId = _listfileReader.GetFileDataIdByEntry($"dbfilesclient/{normalizedName.name}.db2");
+
+                var dbdInfo = DbdBuilder.Build(dbdStream, normalizedName.displayName, version, fileDataId);
+
+                DB2StructResult.Text = dbdInfo.GetColumns();
+                DB2MetaResult.Text = dbdInfo.DumpMeta();
+            }
+            catch
             {
-                ResultBox.Text = $"Couldn't retrieve data for {tableName}!";
+                _ = MessageBox.Show($"Couldn't retrieve data for {normalizedName.displayName}!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        private void OpenPath_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = ".csv",
+                Filter = "CSV Files (*.csv)|*.csv"
+            };
+
+            var result = dialog.ShowDialog() ?? false;
+            if (result)
+            {
+                Task.Run(async () =>
+                {
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        ListfilePath.Text = dialog.FileName;
+                        DumpButton.IsEnabled = false;
+                    });
+
+                    // Load the listfile csv
+                    await _listfileReader.ReadFileAsync(dialog.FileName);
+
+                    // Populate the dropdown with db2 names
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        TableNameInputGrid.Visibility = Visibility.Hidden;
+                        TableNameDropdownGrid.Visibility = Visibility.Visible;
+
+                        var db2s = _listfileReader.GetAvailableDB2s();
+                        foreach (var db2 in db2s)
+                        {
+                            var tableName = db2.Value.Replace("dbfilesclient/", "").Replace(".db2", "");
+
+                            var normalizedName = _db2DisplayNames.FirstOrDefault(x => x.name == tableName);
+                            if (normalizedName != null)
+                                TableDropdown.Items.Add(normalizedName.displayName);
+                        }
+
+                        DumpButton.IsEnabled = true;
+                    });
+
+                    _ = MessageBox.Show("Finished loading listfile!");
+                });
+            }
+        }
+
+        private void LoadDisplayNames()
+        {
+            var fileStream = File.OpenRead("db2_display_names.json");
+
+            var content = JsonSerializer.Deserialize<List<DB2DisplayNameRecord>>(fileStream);
+            if (content == null)
+            {
+                _ = MessageBox.Show("Failed to load DB2 Display Names", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            _db2DisplayNames = content;
+        }
     }
+
+    public record DB2DisplayNameRecord(string name, string displayName);
 }
